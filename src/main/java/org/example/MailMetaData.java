541ef87bf97f1fc62ed9a4008bc5a00693f1de50
banasiddhaPatil <banasiddhapatil@gmail.com>
2023-06-11 20:24:10 +0530

    fist commit

---
+++ src/main/java/org/example/MailMetaData.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class MailMetaData {
+
+    public static final String HostServer = "smtp.gmail.com";
+    public static final String port = "465";
+    public static final String sslProperty = "mail.smtp.ssl.enable";
+    public static final String authPerm = "mail.smtp.auth";
+
+    public static final String myUserMail = "banasiddhapatil@gmail.com";
+
+    public static final String password = "zvgrimhormwwbcce";// app password
+
+    public static final String receiverMail = "patilbansiddha@gmail.com";
+
+
+}
